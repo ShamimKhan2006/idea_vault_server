910ac3994e19525cea753939d1c2110e241e220a
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-05-19 23:44:08 +0600

    login form updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =process.env.PORT || 8000
 const cors=require("cors")
 require("dotenv").config();
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
 const uri =process.env.MONGODB_URL;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -31,19 +31,39 @@ async function run() {
     // await client.db("admin").command({ ping: 1 });
      const db=client.db("idea_vault")
      const idea_vaultCollection=db.collection("idea_vault_collection")
-
+     const registerCollection=db.collection("register")
+      app.get("/ideas",async(req,res)=>{
+  
+        const result=await idea_vaultCollection.find().toArray()
+       const  dd= await  res.send(result)
+        console.log(dd)
+      })
       
       app.post("/regis",async (req,res)=>{
         const user=req.body
 
-        const result=await idea_vaultCollection.insertOne(user)
+        const result=await registerCollection.insertOne(user)
         res.send(result)
       })
+         
+     app.post("/Addideas", async(req,res)=>{
+        ideas=req.body
+
+      const result=await idea_vaultCollection.insertOne(ideas)
+      res.json(result)
+     } )
+     
+
+     app.get("/ideas/:id",async (req,res)=>{
+      const {id}=req.params
+      const query={_id : new ObjectId(id)}
+      const result=await idea_vaultCollection.findOne(query)
+        await res.send(result)
+     })
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
 run().catch(console.dir);
