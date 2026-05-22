6b8139e9039e890cfe858ab24b2c0eb6ce682923
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-05-22 15:07:05 +0600

    backend secure

--- index.js
+++ index.js
@@ -2,15 +2,37 @@ const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =process.env.PORT || 8000
-const cors=require("cors")
+const cors=require("cors");
+const { createRemoteJWKSet, jwtVerify } = require('jose-node-cjs-runtime');
 require("dotenv").config();
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+const JWKS = createRemoteJWKSet(
+      new URL('http://localhost:3000/api/auth/jwks')
+)
+const verifyToken= async(req,res,next)=>{
+  const authHeaders=req?.headers.authorization
+   if(!authHeaders){
+    return res.state(401).json({message:"Unauthorization"})
+  }
+ 
+  const token=authHeaders.split(" ")[1]
+  if(!token){
+    return res.state(401).json({message:"Unauthorization"})
+  }
 
-
+    const { payload } = await jwtVerify(token, JWKS)
+      try {
+        console.log("payload", payload)
+        next()
+        
+      } catch (error) {
+        return res.state(403).json({message:"Forbidden"})
+      }
+ }
 
 const uri =process.env.MONGODB_URL;
 
@@ -32,11 +54,18 @@ async function run() {
      const db=client.db("idea_vault")
      const idea_vaultCollection=db.collection("idea_vault_collection")
      const registerCollection=db.collection("register")
+     const limi_ideasColl=db.collection("limit_ideas")
       app.get("/ideas",async(req,res)=>{
   
         const result=await idea_vaultCollection.find().toArray()
        const  dd= await  res.send(result)
         console.log(dd)
+      })
+
+      app.get("/featured",async (req,res)=>{
+        const result=await idea_vaultCollection.find().limit(6).toArray()
+       const home= await res.send(result)
+       console.log("home",home)
       })
       
       app.post("/regis",async (req,res)=>{
@@ -46,7 +75,7 @@ async function run() {
         res.send(result)
       })
          
-     app.post("/Addideas", async(req,res)=>{
+     app.post("/Addideas", verifyToken, async(req,res)=>{
         ideas=req.body
 
       const result=await idea_vaultCollection.insertOne(ideas)
@@ -54,7 +83,8 @@ async function run() {
      } )
      
 
-     app.get("/ideas/:id",async (req,res)=>{
+     app.get("/ideas/:id",verifyToken, async (req,res)=>{
+
       const {id}=req.params
       const query={_id : new ObjectId(id)}
       const result=await idea_vaultCollection.findOne(query)
@@ -72,3 +102,5 @@ run().catch(console.dir);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
